1b8d607163c05297326df852f234dd1c847b463b
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-01-04 17:24:09 +0100

    [MODIFY] begin to add verif

--- client/src/components/Connexion.jsx
+++ client/src/components/Connexion.jsx
@@ -3,15 +3,31 @@ import React, { useState } from "react";
 export default function Connexion() {
     const [option, setOption] = useState('connexion')
 
+    function verifForm(object){
+        const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+        const email = document.querySelector('#email')
+        // const password = object.target.children[0].children[1].value
+        console.log(email)
+        if(object.target.id === 'connexion'){
+            if(email.value.match(regexEmail)){
+                // console.log(object.target.children[0].children[0].value.match(regexEmail))
+                console.log('OK')
+            }
+        }
+    }
+
     function ConnexionForm(){
         return (
             <div className='connexion-form'>
-                <form action="POST">
-                    <label htmlFor="email">Email : <input type="email" /></label>
-                    <label htmlFor="password">Password : <input type="password"/></label>
+                <form id="connexion" onSubmit={(e) => {
+                    e.preventDefault();
+                    verifForm(e);
+                }}>
+                    <label htmlFor="email">Email : <input type="email" id="email" placeholder="Email"/></label>
+                    <label htmlFor="password">Password : <input type="password" id="password" placeholder="Mot de passe"/></label>
                     <button type="submit">Connexion</button>
                 </form>
-                <p>Vous n'avez pas de compte ? <button onClick={() => setOption('inscription')}>Inscrivez vous</button>.</p>
+                <p>Vous n'avez pas de compte ? <button onClick={() => setOption('inscription')}>Inscrivez vous</button></p>
             </div>
                 
         )
@@ -19,16 +35,19 @@ export default function Connexion() {
 
     function InscriptionForm(){
         return (
-            <div className="inscription-form">
-                <form action="POST">
-                    <label htmlFor="firstname">Prénom : <input type="text" /></label>
-                    <label htmlFor="lastname">Nom : <input type="text" /></label>
-                    <label htmlFor="email">Email : <input type="email" name="" id="" /></label>
-                    <label htmlFor="password">Mot de passe : <input type="password" id="password" /></label>
-                    <label htmlFor="confirm-password">Confirmez le mot de passe : <input type="password" id="confirm-password" /></label>
+            <div>
+                <form id="inscription" onSubmit={(e) => {
+                    e.preventDefault();
+                    verifForm(e);
+                }}>
+                    <label htmlFor="firstname">Prénom : <input type="text" placeholder="Prenom"/></label>
+                    <label htmlFor="lastname">Nom : <input type="text" placeholder="Nom"/></label>
+                    <label htmlFor="email">Email : <input type="email" id="email" placeholder="Email"/></label>
+                    <label htmlFor="password">Mot de passe : <input type="password" id="password" placeholder="Mot de passe"/></label>
+                    <label htmlFor="confirm-password">Confirmez le mot de passe : <input type="password" id="confirm-password" placeholder="Confirmez le mot de passe"/></label>
                     <button type="submit">Inscrire</button>
                 </form>
-                <p>Vous avez déjà un compte ? <button onClick={() => setOption('connexion')}>Connectez vous</button>.</p>
+                <p>Vous avez déjà un compte ? <button onClick={() => setOption('connexion')}>Connectez vous</button></p>
             </div>
         )
     }
