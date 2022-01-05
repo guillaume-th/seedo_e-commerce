9d91eff80b6be01aaaa6a2389ba677286a27c0b7
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-01-05 15:23:51 +0100

    [MODIFY] finish verif before submiting form

--- client/src/components/Connexion.jsx
+++ client/src/components/Connexion.jsx
@@ -3,92 +3,111 @@ import React, { useState } from "react";
 export default function Connexion() {
     const [option, setOption] = useState('connexion')
 
-    function verifInscriptionForm(object){
+    function verifInscriptionForm(object) {
+        const verified = []
+        const mustBeVerified = ['firstname', 'lastname', 'email', 'password', 'passwordVerif']
         const firstname = document.querySelector('#firstname');
         const lastname = document.querySelector('#lastname');
         const email = document.querySelector('#email');
         const password = document.querySelector('#password');
         const passwordVerif = document.querySelector('#confirm-password');
         const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
-        if(object.target.id === 'inscription'){
+        if (object.target.id === 'inscription') {
             // firstname check
-            if (firstname.value.length > 30 || firstname.value.length < 1 || firstname.value.match(/\d/) || firstname.value.match(/\W/)){
+            if (firstname.value.length > 30 || firstname.value.length < 1 || firstname.value.match(/\d/) || firstname.value.match(/\W/)) {
                 firstname.style.backgroundColor = 'red'
+                verified[0] = ''
             } else {
                 firstname.style.backgroundColor = 'green'
+                verified[0] = 'firstname'
             }
             // lastname check
-            if (lastname.value.length > 30 || lastname.value.length < 1 || lastname.value.match(/\d/) || lastname.value.match(/\W/)){
+            if (lastname.value.length > 30 || lastname.value.length < 1 || lastname.value.match(/\d/) || lastname.value.match(/\W/)) {
                 lastname.style.backgroundColor = 'red'
+                verified[1] = ''
             } else {
                 lastname.style.backgroundColor = 'green'
+                verified[1] = 'lastname'
             }
             // email check
-            if(!email.value.match(regexEmail)){
+            if (!email.value.match(regexEmail)) {
                 email.style.backgroundColor = 'red'
+                verified[2] = ''
             } else {
                 email.style.backgroundColor = 'green'
+                verified[2] = 'email'
             }
             // password check
-            if (password.value.length > 50 || password.value.length < 8){
+            if (password.value.length > 50 || password.value.length < 8) {
                 password.style.backgroundColor = 'red'
+                verified[3] = ''
             } else {
                 password.style.backgroundColor = 'green'
+                verified[3] = 'password'
                 // verif password check
-                if(passwordVerif.value === password.value){
-                    passwordVerif.style.backgroundColor = 'green'
-                } else {
+                if (passwordVerif.value !== password.value) {
                     passwordVerif.style.backgroundColor = 'red'
+                    verified[4] = ''
+                } else {
+                    passwordVerif.style.backgroundColor = 'green'
+                    verified[4] = 'passwordVerif'
                 }
             }
         }
+        if(JSON.stringify(verified) === JSON.stringify(mustBeVerified)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    function levelPassword(password){
+    function levelPassword(password) {
         let level = 0;
-        if(password.match(/[A-Z]/)){
+        if (password.match(/[A-Z]/)) {
             level++
         }
-        if(password.match(/[a-z]/)){
+        if (password.match(/[a-z]/)) {
             level++
         }
-        if(password.match(/\W/)){
+        if (password.match(/\W/)) {
             level++
         }
-        if(password.match(/\d/)){
+        if (password.match(/\d/)) {
             level++
         }
         return level
     }
 
-    function ConnexionForm(){
+    function ConnexionForm() {
         return (
             <div className='connexion-form'>
                 <form id="connexion" onSubmit={(e) => {
                     e.preventDefault();
                 }}>
-                    <label htmlFor="email">Email : <input type="email" id="email" placeholder="Email"/></label>
-                    <label htmlFor="password">Password : <input type="password" id="password" placeholder="Mot de passe"/></label>
+                    <label htmlFor="email">Email : <input type="email" id="email" placeholder="Email" /></label>
+                    <label htmlFor="password">Password : <input type="password" id="password" placeholder="Mot de passe" /></label>
                     <button type="submit">Connexion</button>
                 </form>
                 <p>Vous n'avez pas de compte ? <button onClick={() => setOption('inscription')}>Inscrivez vous</button></p>
             </div>
-                
+
         )
     }
 
-    function InscriptionForm(){
+    function InscriptionForm() {
         return (
             <div>
                 <form id="inscription" onSubmit={(e) => {
                     e.preventDefault();
-                    verifInscriptionForm(e);
+                    if(verifInscriptionForm(e)){
+                        e.target.submit()
+                    }
                 }}>
-                    <label htmlFor="firstname">Prénom : <input type="text" id="firstname" placeholder="Prenom" minLength={1} maxLength={30}/></label>
-                    <label htmlFor="lastname">Nom : <input type="text" id="lastname" placeholder="Nom" minLength={1} maxLength={30}/></label>
-                    <label htmlFor="email">Email : <input type="text" id="email" placeholder="Email"/></label>
-                    <label htmlFor="password">Mot de passe : <input type="password" id="password" placeholder="Mot de passe" onChange={(e)=>{levelPassword(e.target.value)}}/></label>
-                    <label htmlFor="confirm-password">Confirmez le mot de passe : <input type="password" id="confirm-password" placeholder="Confirmez le mot de passe"/></label>
+                    <label htmlFor="firstname">Prénom : <input type="text" id="firstname" placeholder="Prenom" minLength={1} maxLength={30} /></label>
+                    <label htmlFor="lastname">Nom : <input type="text" id="lastname" placeholder="Nom" minLength={1} maxLength={30} /></label>
+                    <label htmlFor="email">Email : <input type="text" id="email" placeholder="Email" /></label>
+                    <label htmlFor="password">Mot de passe : <input type="password" id="password" placeholder="Mot de passe" onChange={(e) => { levelPassword(e.target.value) }} /></label>
+                    <label htmlFor="confirm-password">Confirmez le mot de passe : <input type="password" id="confirm-password" placeholder="Confirmez le mot de passe" /></label>
                     <button type="submit">Inscrire</button>
                 </form>
                 <p>Vous avez déjà un compte ? <button onClick={() => setOption('connexion')}>Connectez vous</button></p>
