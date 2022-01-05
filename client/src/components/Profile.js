32afed062d5504d0cb3fe37d43f242e611480912
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2022-01-05 12:49:31 +0100

    [ADD] router in App.js, beginning of Profile.js

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,13 +1,19 @@
 import { useRef, useEffect, useState } from "react";
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export default function Profile() {
     const userForm = useRef();
     const [user, setUser] = useState(null);
+    const user_id = localStorage.getItem("user_id");
 
     useEffect(() => {
-        const user_id = localStorage.getItem("user_id");
-        fetch(`${API_URL}/user/${user_id}`)
+        console.log("in", `${API_URL}/user/${user_id}`); 
+        fetch(`${API_URL}/user/${user_id}`, 
+        {
+            method : "GET", 
+            "Access-Control-Allow-Origin" : "*",
+        })
+
             .then((res) => res.json())
             .then((res) => setUser(res))
             .catch((err) => console.error(err));
@@ -29,11 +35,46 @@ export default function Profile() {
             })
             .catch((error) => console.error(error));
     }
-    return (
-        <form ref={userForm} onSubmit={submitUserData}>
-
-        </form>
-    );
+    if(user){
+        return (
+            <div>
+                <form ref={userForm} onSubmit={submitUserData}>
+                    <input type="text" name="firstname" placeholder="Prénom" defaultValue={user.firstname}></input>
+                    <input type="text" name="lastname" placeholder="Nom de famille" defaultValue={user.lastname}></input>
+                    <input type="text" name="email" placeholder="Email" defaultValue={user.email}></input>
+                    <input type="text" name="telephone" placeholder="Téléphone" defaultValue={user.email}></input>
+                    <input type="password" name="password" placeholder="Password"></input>
+                    <h3>Coordonnées bancaires</h3>
+                    <input type="text" name="number_CB" placeholder="Numéro de carte bancaire"></input>
+                    <input type="text" name="cvv" placeholder="CVV"></input>
+                    <input type="date" name="expiration_CB" placeholder="Date d'expiration"></input>
+                    <input type="submit" value="Sauvegarder les modifications"></input>
+                </form>
+                <h3>Vos adresses</h3>
+                {
+                    user.addresses.map((e) => {
+                        return (
+                            <div>
+                                <p>{e.number} {e.street} </p>
+                                <p>{e.postal_code}, {e.country}</p>
+                                <button>Modify</button>
+                                <hr></hr>
+                            </div>
+                        );
+                    })
+                }
+                {/* <form ref={adressForm}>
+                    <input></input>
+                </form> */}
+            </div>
+    
+        );
+    }
+    else{
+        return(
+            <p>Vous n'êtes pas connecté.</p>
+        );
+    }
 
 
 }
