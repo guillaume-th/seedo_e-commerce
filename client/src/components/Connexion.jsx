88dfe7420f0b384f4700282e2f3ca5b541680549
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-01-05 15:06:48 +0100

    [MODIFY] finish verif before sending form

--- client/src/components/Connexion.jsx
+++ client/src/components/Connexion.jsx
@@ -11,9 +11,35 @@ export default function Connexion() {
         const passwordVerif = document.querySelector('#confirm-password');
         const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
         if(object.target.id === 'inscription'){
-            if(email.value.match(regexEmail)){
-                // console.log(object.target.children[0].children[0].value.match(regexEmail))
-                console.log(level)
+            // firstname check
+            if (firstname.value.length > 30 || firstname.value.length < 1 || firstname.value.match(/\d/) || firstname.value.match(/\W/)){
+                firstname.style.backgroundColor = 'red'
+            } else {
+                firstname.style.backgroundColor = 'green'
+            }
+            // lastname check
+            if (lastname.value.length > 30 || lastname.value.length < 1 || lastname.value.match(/\d/) || lastname.value.match(/\W/)){
+                lastname.style.backgroundColor = 'red'
+            } else {
+                lastname.style.backgroundColor = 'green'
+            }
+            // email check
+            if(!email.value.match(regexEmail)){
+                email.style.backgroundColor = 'red'
+            } else {
+                email.style.backgroundColor = 'green'
+            }
+            // password check
+            if (password.value.length > 50 || password.value.length < 8){
+                password.style.backgroundColor = 'red'
+            } else {
+                password.style.backgroundColor = 'green'
+                // verif password check
+                if(passwordVerif.value === password.value){
+                    passwordVerif.style.backgroundColor = 'green'
+                } else {
+                    passwordVerif.style.backgroundColor = 'red'
+                }
             }
         }
     }
@@ -40,7 +66,6 @@ export default function Connexion() {
             <div className='connexion-form'>
                 <form id="connexion" onSubmit={(e) => {
                     e.preventDefault();
-                    // verifForm(e);
                 }}>
                     <label htmlFor="email">Email : <input type="email" id="email" placeholder="Email"/></label>
                     <label htmlFor="password">Password : <input type="password" id="password" placeholder="Mot de passe"/></label>
@@ -59,15 +84,14 @@ export default function Connexion() {
                     e.preventDefault();
                     verifInscriptionForm(e);
                 }}>
-                    <label htmlFor="firstname">Prénom : <input type="text" id="firstname" placeholder="Prenom"/></label>
-                    <label htmlFor="lastname">Nom : <input type="text" id="lastname" placeholder="Nom"/></label>
+                    <label htmlFor="firstname">Prénom : <input type="text" id="firstname" placeholder="Prenom" minLength={1} maxLength={30}/></label>
+                    <label htmlFor="lastname">Nom : <input type="text" id="lastname" placeholder="Nom" minLength={1} maxLength={30}/></label>
                     <label htmlFor="email">Email : <input type="text" id="email" placeholder="Email"/></label>
                     <label htmlFor="password">Mot de passe : <input type="password" id="password" placeholder="Mot de passe" onChange={(e)=>{levelPassword(e.target.value)}}/></label>
                     <label htmlFor="confirm-password">Confirmez le mot de passe : <input type="password" id="confirm-password" placeholder="Confirmez le mot de passe"/></label>
                     <button type="submit">Inscrire</button>
                 </form>
                 <p>Vous avez déjà un compte ? <button onClick={() => setOption('connexion')}>Connectez vous</button></p>
-                {/* <p>{passwordLevel}</p> */}
             </div>
         )
     }
