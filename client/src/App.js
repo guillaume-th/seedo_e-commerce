43decb6d423933a3b6ccfa51280a94e4a487829f
Nolwenn <nolwenn.sibiril@epitech.eu>
2022-01-06 15:59:34 +0100

    [Fix] git conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes, HashRouter as Router } from "react-router-dom";
 import Profile from './components/Profile';
+import ListArticles from './components/ListArticles';
 
 function App() {
   return (  
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<div>Hello</div>}/>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/graines" element={<ListArticles />} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/components/ListArticles.js
@@ -0,0 +1,11 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+export default function ListArticles(){
+    //const user_id = localStorage.getItem("user_id");
+
+    fetch(`${API_URL}/articles`, { method: "GET" })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+   // console.log('Process  :'+process.env.REACT_APP_API_URL)
+}
