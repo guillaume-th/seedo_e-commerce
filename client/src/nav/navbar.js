b2a2990b5eaf688f98eb3c5775da524343936251
Wassim8035 <wassim.horri@epitech.eu>
2022-01-06 16:43:29 +0100

    [MODIFY] navbar components

--- client/src/nav/navbar.js
+++ client/src/nav/navbar.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import Marron from './marron.jpeg';
+import Graine from './seed.png';
+import User from './user.svg';
+import Shop from './shop.svg';
+import Title from './title.png';
+import Search from './search.svg'
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Navbar() {
 
     return (
-    <div className='back'> 
-      <nav class="navbar navbar-expand-lg">
-         
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">LOGO</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
- <form class="d-flex">
-        <input class="form-control me-sm-2" type="text" placeholder="Search"/>
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-   
-  </div>
-</nav>
-    </div>
+        <div className='back'>
+            <nav class="navbar navbar-expand-lg">
+                
+               
+                <div class="container-fluid">
+                    <div class="logo">
+                   
+                     <img class="graine" src={Graine} alt="Seed_logo" />
+                     <img class="title" src={Title} alt="" />
+                   
+                     
+                     </div>
+     
+                    <form class="d-flex search_bar px-0">
+                    <input class="form-control search pr-0" type="text" placeholder="Search"/>
+                    <button class="btn but"><img class="loupe" src={Search} alt="search_logo"/></button>
+                    </form>
+                    <div class="icon">
+                    <img class="Shop" src={Shop} alt="Shop_logo" />
+                    <img class="User" src={User} alt="User_logo" />
+                    </div>
+                </div>
+            </nav>
+        </div>
     )
 }
