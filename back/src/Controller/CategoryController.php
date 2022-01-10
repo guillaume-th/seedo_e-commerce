dd00400a20b2e8078efcc8a84d36d62174d0a8f2
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-01-10 14:50:31 +0100

    [MODIFY] category->getAll()

--- back/src/Controller/CategoryController.php
+++ back/src/Controller/CategoryController.php
@@ -17,77 +17,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/", name="category_index", methods={"GET"})
+     * @Route("/all", name="category_index", methods={"GET"})
      */
-    public function index(CategoryRepository $categoryRepository): Response
+    public function getAll(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="category_new", methods={"GET", "POST"})
-     */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($category);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('category_index', [], Response::HTTP_SEE_OTHER);
+        $request = $categoryRepository->findAll();
+        $result = [];
+        for ($i = 0; $i < count($request); $i++) {
+            array_push($result, ['id' => $request[$i]->getId(), 'name' => $request[$i]->getName()]);
         }
-
-        return $this->renderForm('category/new.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="category_show", methods={"GET"})
-     */
-    public function show(Category $category): Response
-    {
-        return $this->render('category/show.html.twig', [
-            'category' => $category,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="category_edit", methods={"GET", "POST"})
-     */
-    public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('category_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('category/edit.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="category_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($category);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('category_index', [], Response::HTTP_SEE_OTHER);
+        return $this->json($result);
     }
 }
