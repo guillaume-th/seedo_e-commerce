3438f39fe57a70fca80316a724f661a1c6e9ac2f
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-01-11 17:18:21 +0100

    [MODIFY] CRUD front side

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -2,9 +2,11 @@ import React, { useState, useRef, useEffect } from "react";
 const API_URL = process.env.REACT_APP_API_URL;
 
 export default function Category() {
+  const [data, setData] = useState(null);
   const createForm = useRef();
   const updateForm = useRef();
-  const categories = [];
+  const deleteForm = useRef();
+  const [refresh, setRefresh] = useState(null);
 
   useEffect(() => {
     fetch(`${API_URL}/category/all`, {
@@ -12,39 +14,117 @@ export default function Category() {
       // body: data,
     })
       .then((res) => res.json())
-      .then((res) => console.log(res[0]))
+      .then((res) => setData(res))
       .catch((error) => console.error(error));
-  }, [categories]);
+  }, [refresh]);
 
-  return (
-    <>
-      <section>
-        <h2>Create</h2>
-        <form ref={createForm} encType="multipart/form-data">
-          <label htmlFor="name">
-            <input name="name" type="text" placeholder="Nom catégorie" />
-          </label>
-          <button type="submit">Creer</button>
-        </form>
-      </section>
-      <section>
-        <h2>Read</h2>
-      </section>
-      <section>
-        <h2>Update</h2>
-        <form ref={updateForm} encType="multipart/form-data">
-          <label htmlFor="id">
-            <input name="id" type="text" placeholder="id" />
-          </label>
-          <label htmlFor="name">
-            <input name="name" type="text" placeholder="Nouveau nom" />
-          </label>
-          <button type="submit"></button>
-        </form>
-      </section>
-      <section>
-        <h2>Delete</h2>
-      </section>
-    </>
-  );
+  function createCategory(name) {
+    fetch(`${API_URL}/category/new`, {
+      method: "POST",
+      body: name,
+    })
+      .then((res) => res.json())
+      .then(() => setRefresh(Math.random()))
+      .catch((error) => console.error(error));
+  }
+
+  function updateCategory(dataForm) {
+    const id = dataForm.get("id");
+    fetch(`${API_URL}/category/${id}/edit`, {
+      method: "POST",
+      body: dataForm,
+    })
+      .then((res) => res.json())
+      .then(() => setRefresh(Math.random()))
+      .catch((error) => console.error(error));
+  }
+
+  function deleteCategory(dataForm) {
+    const id = dataForm.get("id");
+    fetch(`${API_URL}/category/delete/${id}`, {
+      method: "POST",
+      body: dataForm,
+    })
+      .then((res) => res.json())
+      .then(() => setRefresh(Math.random()))
+      .catch((error) => console.error(error));
+  }
+
+  if (data) {
+    return (
+      <>
+        <section>
+          <h2>Create</h2>
+          <form
+            ref={createForm}
+            encType="multipart/form-data"
+            onSubmit={(e) => {
+              e.preventDefault();
+              let name = new FormData(createForm.current);
+              createCategory(name);
+            }}
+          >
+            <label htmlFor="name">
+              <input name="name" type="text" placeholder="Nom catégorie" />
+            </label>
+            <button type="submit">Creer</button>
+          </form>
+        </section>
+        <section>
+          <h2>Read</h2>
+          <ul>
+            {data.map((e) => {
+              // eslint-disable-next-line
+              return (
+                <>
+                  <li>
+                    Id : {e.id} &nbsp; Categorie : {e.name}
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+        </section>
+        <section>
+          <h2>Update</h2>
+          <form
+            ref={updateForm}
+            encType="multipart/form-data"
+            onSubmit={(e) => {
+              e.preventDefault();
+              let current = new FormData(updateForm.current);
+              updateCategory(current);
+            }}
+          >
+            <label htmlFor="id">
+              <input name="id" type="text" placeholder="id" />
+            </label>
+            <label htmlFor="name">
+              <input name="name" type="text" placeholder="Nouveau nom" />
+            </label>
+            <button type="submit"></button>
+          </form>
+        </section>
+        <section>
+          <h2>Delete</h2>
+          <form
+            ref={deleteForm}
+            encType="multipart/form-data"
+            onSubmit={(e) => {
+              e.preventDefault();
+              let current = new FormData(deleteForm.current);
+              deleteCategory(current);
+            }}
+          >
+            <label>
+              <input name="id" type="text" />
+            </label>
+            <button type="submit">Supprimer</button>
+          </form>
+        </section>
+      </>
+    );
+  } else {
+    return <p>Chargement en cours...</p>;
+  }
 }
