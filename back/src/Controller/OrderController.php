09d3e94821cc4b512d83abd6797c2f2bc7439d57
wac <guillaume.thiebaud@epitech.eu>
2022-01-11 14:22:12 +0100

    [add]crud order

--- back/src/Controller/OrderController.php
+++ back/src/Controller/OrderController.php
@@ -67,6 +67,9 @@ class OrderController extends AbstractController
         $adress = $this->getDoctrine()->getRepository(Adress::class)->findOneBy([
             "street" => $request["adress"]["street"],
             "country" => $request["adress"]["country"],
+            "postal_code" => $request["adress"]["postal_code"],
+            "number" => $request["adress"]["number"],
+            "city" => $request["adress"]["city"],
         ]);
         $Order->setAdress($adress);
         foreach ($request["articles_id"] as $value) {
@@ -74,6 +77,9 @@ class OrderController extends AbstractController
             if ($quantity->getQuantity() < $value['quantity']) {
                 $data["status"] = "out of stock";
                 return $this->json($data);
+            }else{
+                $quantityfinal  = $quantity->getQuantity() - $value['quantity'];
+                $quantity->setQuantity($quantityfinal);
             }
             for ($i = 0; $i < $value['quantity']; $i++) {
                 $article = $this->getDoctrine()->getRepository(Article::class)->find($value['id']);
@@ -88,10 +94,108 @@ class OrderController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="order_selected" , methods={"POST"})
+     * @Route("/select/{id}", name="order_selected" , methods={"POST"})
      */
     public function SelectedOrder(Request $request, Order $order,  EntityManagerInterface $entityManager): Response
     {
-        dd($order);
+        $order =  $this->getDoctrine()->getRepository(Order::class)->find($order->getId());
+        $data = [];
+        $orderarticle = [];
+            foreach ($order->getArticles() as $value) {
+                array_push($orderarticle, [
+                    'id' => $value->getId(),
+                    'name' => $value->getName(),
+                    'price' => $value->getPrice(),
+                ]);
+            }
+            array_push($data, [
+                "id" => $order->getId(),
+                "status" => $order->getStatus(),
+                "creation_date" => $order->getCreationDate(),
+                "user" => [
+                    "id_user" => $order->getUser()->getId(),
+                    "email_user" => $order->getUser()->getEmail(),
+                    "firstname_user" => $order->getUser()->getFirstname(),
+                    "lastname_user" => $order->getUser()->getLastname(),
+                ],
+                "article" => $orderarticle,
+                "OrderPrice" => $order->getOrderPrice()
+            ]);
+        return $this->json(['result' => $data]);
+    }
+    /**
+     * @Route("/user/{id}", name="order_user" , methods={"GET"})
+     */
+    public function SelectedUser(Request $request, User $user): Response
+    {
+        $orders =  $this->getDoctrine()->getRepository(Order::class)->findBy(["user" => $user->getId()]);
+        $data = [];
+        $orderarticle = [];
+        foreach ($orders as $order) {
+            foreach ($order->getArticles() as $value) {
+                array_push($orderarticle, [
+                    'id' => $value->getId(),
+                    'name' => $value->getName(),
+                    'price' => $value->getPrice(),
+                ]);
+            }
+            array_push($data, [
+                "id" => $order->getId(),
+                "status" => $order->getStatus(),
+                "creation_date" => $order->getCreationDate(),
+                "user" => [
+                    "id_user" => $order->getUser()->getId(),
+                    "email_user" => $order->getUser()->getEmail(),
+                    "firstname_user" => $order->getUser()->getFirstname(),
+                    "lastname_user" => $order->getUser()->getLastname(),
+                ],
+                "article" => $orderarticle,
+                "OrderPrice" => $order->getOrderPrice()
+            ]);
+        }
+        return $this->json(['result' => $data]);
+    }
+      /**
+     * @Route("/edit/{id}", name="order", methods={"POST"})
+     */
+    public function order_edit(Order $order,Request $request): Response
+    {
+        $request = json_decode($request->getContent(), true); 
+        $order->setStatus($request["status"]);
+        $data = [];
+        $orderarticle = [];
+            foreach ($order->getArticles() as $value) {
+                array_push($orderarticle, [
+                    'id' => $value->getId(),
+                    'name' => $value->getName(),
+                    'price' => $value->getPrice(),
+                ]);
+            }
+            array_push($data, [
+                "id" => $order->getId(),
+                "status" => $order->getStatus(),
+                "creation_date" => $order->getCreationDate(),
+                "user" => [
+                    "id_user" => $order->getUser()->getId(),
+                    "email_user" => $order->getUser()->getEmail(),
+                    "firstname_user" => $order->getUser()->getFirstname(),
+                    "lastname_user" => $order->getUser()->getLastname(),
+                ],
+                "article" => $orderarticle,
+                "OrderPrice" => $order->getOrderPrice()
+            ]);
+        return $this->json(['result' => $data]);
+    }
+
+       /**
+     * @Route("/remove/{id}", name="order_remove", methods={"POST"})
+     */
+    public function remove_order(Order $order,EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($order);
+        $entityManager->flush();
+      
+        $data="ok";
+        return $this->json(['result' => $data]);
     }
 }
