b2492421e530dc0531f7bab44511c8e0f7cf21ce
Nolwenn <nolwenn.sibiril@epitech.eu>
2022-01-13 15:52:09 +0100

    [update] App.css/ArticleDescription.js - style for the component

--- client/src/components/ArticleDescription.js
+++ client/src/components/ArticleDescription.js
@@ -7,6 +7,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export default function ArticleDetail() {
     const [data, setData] = useState(null);
+    const [imgFirstLink, setImgFirstLink] = useState(null);
     const { id } = useParams();
     const cart = useSelector((state => state.cart.value));
     const dispatch = useDispatch();
@@ -16,6 +17,8 @@ export default function ArticleDetail() {
             .then(res => res.json())
             .then(res => {
                 setData(res.data);
+                setImgFirstLink(res.data.photos[0].imgLink);
+                // console.log(res.data.photos[0].imgLink)
             })
             .catch(err => console.error(err));
     }, []);
@@ -43,40 +46,56 @@ export default function ArticleDetail() {
         return Math.round(parseFloat(e.promo > 0 ? e.price - (e.price * e.promo / 100) : e.price), 2);
     }
 
+    const switchPhoto = (e) => {
+        setImgFirstLink(e.target.src);
+    }
+
+
     if (data) {
+    // console.log(data.photos[0].imgLink);
         return (
             <div>
                 <div key={data.id} id="ficheDetail">
                     <h2 className="titleName">{data.name}</h2>
+                    <p>{data.description}</p>
+                    <p><strong className="green">Couleur : </strong>{data.color ? data.color : 'non renseignée' }</p>
+                    <div className="infoDetail">
+                        <p><strong className="green">Prix : </strong>{data.price} €</p>
+                        <p><strong className="green">Poids : </strong>{data.weight}g</p>
+                    </div>
                     <form onSubmit={(event) => addToCart(event, data)}>
-                        <input type="number" id={data.id} defaultValue={1}></input>
+                        <input type="number" id={data.id} defaultValue={1} className="number"></input>
                         <input type="submit" value="Ajouter au panier" />
                     </form>
                     <p>{data.categoriesName}</p>
-                    <p>photo : </p>
-                    <ul className="photos">
+                    <div className="photos">
+                        <div className="lgPhoto">
+                            <img src={imgFirstLink} className="imgPrincipale"></img>
+                        </div>
+                        <div className="smPhotos">
                         {
                             data.photos.map(i => {
-                                console.log(i.id);
-                                if(i.id == 1){
+                                if(i.imgLink !== imgFirstLink){
                                     return (
                                         i &&
-                                        <img key={i.id} src={i.imgLink} className="imgPrincipal"></img>
-                                    )
-                                }else{
-                                    return (
-                                        i &&
-                                        <img key={i.id} src={i.imgLink} className="imgSecondaire"></img>
+                                        <div onClick={switchPhoto} className="imgSecondaire"><img key={i.id} src={i.imgLink} className="imgSecondaire"></img></div>
                                     )
                                 }
-                                
                             })
                         }
+                        </div>
+                    </div>
+                    <ul id="comments">
+                        <li>Ceci est un commentaire</li>
+                        <li>Ceci est un commentaire</li>
+                        <li>Ceci est un commentaire</li>
+                        <li>Ceci est un commentaire</li>
+                        <li>Ceci est un commentaire</li>
+                        <li>Ceci est un commentaire</li>
                     </ul>
-                    <p>{data.description}</p>
-                    <p>Prix {data.price} €</p>
-                    <p>Poids : {data.weight}</p>
-                    <p>Couleur : {data.color}</p>
+                    <p className="green">Laissez un commentaire à propos de l'article !</p>
+                    <input type="textarea" id="leaveComment" rows='30' cols='30' ></input>
+                    <input type="submit" value="commenter"></input>
                 </div>
             </div>
         );
