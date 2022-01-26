8f4c54c9152ba32d3c842153c4bdf926543c142e
Nolwenn <nolwenn.sibiril@epitech.eu>
2022-01-26 14:09:24 +0100

    [fix] reduce util

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,9 +2,8 @@ export const reduce = (cart, fidel) => {
     let total = 0;
     console.log(cart);
     for (let i = 0; i < cart.length; i++) {
-        fidel ?
-        total += (cart[i].price * cart[i].selectedQuantity)*0.9
-        : total += cart[i].price * cart[i].selectedQuantity;
+        total += cart[i].price * cart[i].selectedQuantity;
     }
+    total *= 0.9;
     return total.toFixed(2);
 };
