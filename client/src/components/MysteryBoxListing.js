b5ef8139193b28130742042d858db7839145cc6b
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2022-01-27 14:53:47 +0100

    [FIX] overflow in mysteryBox listing

--- client/src/components/MysteryBoxListing.js
+++ client/src/components/MysteryBoxListing.js
@@ -30,7 +30,7 @@ export default function MysteryBoxListing() {
     if (data) {
         return (
             <div className="wrapper">
-                <div className="wrapper" style={{ padding: "3rem" }}>
+                <div className="" style={{ padding: "3rem" }}>
                     <div className="std-colored-wrapper">
                         <h3>Une nouvelle manière d'aborder le jardinage</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam velit non lectus suscipit feugiat. Sed consectetur at lectus sed efficitur. Vestibulum quis est non lectus pretium congue in et dui. Proin sit amet metus at tortor efficitur porttitor quis et massa. Curabitur semper eros eget urna ornare pharetra.  </p>
