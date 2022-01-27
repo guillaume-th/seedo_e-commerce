7e9c30cea8dd3947535a56e2a38841c0066525b4
Nolwenn <nolwenn.sibiril@epitech.eu>
2022-01-27 17:16:47 +0100

    [fix] home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,83 +1,56 @@
-import { Carousel } from "react-responsive-carousel";
+import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import img1 from "../assets/1.jpeg";
 import img2 from "../assets/2.jpeg";
 import img3 from "../assets/3.jpeg";
+import boxSeedo from "../assets/boxSeedo.jpg";
+import graines from "../assets/10-graines-bonnes-pour-la-sante.jpg";
 
 export default function Home() {
-  return (
-    <div>
-      <Carousel
-        autoPlay={"true"}
-        infiniteLoop={"true"}
-        className="vertical-flex center-flex"
-      >
+    return (
         <div>
-          <Carousel
-            autoPlay={"true"}
-            infiniteLoop={"true"}
-            className="vertical-flex center-flex"
-          >
-            <div>
-              <img src={img1} />
-            </div>
-            <div>
-              <img src={img2} />
-            </div>
-            <div>
-              <img src={img3} />
-            </div>
-          </Carousel>
-          <section className="boxMystere">
-            <h1 className="aboutTitle center-text">
-              <em>Mystery Box</em>
-            </h1>
-            <div className="horizontal-flex center-flex container">
-              <div className="wrapper-rond">
-                <img src={img1} />
-              </div>
-              <p className="textBox">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas dignissim, sem id laoreet varius, mauris sem porttitor
-                orci, et vulputate leo ligula quis risus. Nam imperdiet
-                imperdiet mollis. Mauris elit leo, sollicitudin et elementum
-                vitae, bibendum quis nulla. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit
-              </p>
-            </div>
-          </section>
-          <hr className="hrGreen"></hr>
-          <section className="about vertical-flex marginAuto">
-            <h1 className="aboutTitle">Qui sommes nous ?</h1>
-            <div className="content horizontal-flex">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas dignissim, sem id laoreet varius, mauris sem porttitor
-                orci, et vulputate leo ligula quis risus. Nam imperdiet
-                imperdiet mollis. Mauris elit leo, sollicitudin et elementum
-                vitae, bibendum quis nulla. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Vivamus consequat eu nulla nec
-                commodo. Praesent leo quam, luctus sit amet libero in, mattis
-                efficitur leo. Suspendisse sodales ex non vehicula sagittis.
-                Nulla euismod aliquam erat, ut viverra magna tristique in. In
-                quis dolor a justo gravida sollicitudin. Maecenas vehicula
-                lectus at purus sagittis vulputate. Nunc lobortis nulla nunc, eu
-                tristique erat suscipit in. Quisque lacinia quam tincidunt,
-                faucibus ligula sed, porta urna. Etiam dapibus mauris nulla,
-                eget egestas est varius vitae.
-              </p>
-              <img src={img1} />
-            </div>
-          </section>
+            <Carousel autoPlay={"true"} infiniteLoop={"true"} className="vertical-flex center-flex">
+                <div>
+                    <img src={img1} />
+                </div>
+                <div>
+                    <img src={img2} />
+                </div>
+                <div>
+                    <img src={img3} />
+                </div>
+            </Carousel>
+            <section className='boxMystere'>
+                <h1 className='aboutTitle center-text'><em>Mystery Box</em></h1>
+                <div className='horizontal-flex center-flex container'>
+                    <div className='wrapper-rond'>
+                        <img src={boxSeedo} />
+                    </div>
+                    <p className='textBox'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dignissim, sem id laoreet varius, 
+                        mauris sem porttitor orci, et vulputate leo ligula quis risus. Nam imperdiet imperdiet mollis. 
+                        Mauris elit leo, sollicitudin et elementum vitae, bibendum quis nulla. Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit
+                    </p>
+                </div>
+            </section>
+            <hr className='hrGreen'></hr>
+            <section className="about vertical-flex marginAuto">
+                <h1 className='aboutTitle'>Qui sommes nous ?</h1>
+                <div className="content horizontal-flex">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dignissim, sem id laoreet varius, 
+                        mauris sem porttitor orci, et vulputate leo ligula quis risus. Nam imperdiet imperdiet mollis. 
+                        Mauris elit leo, sollicitudin et elementum vitae, bibendum quis nulla. Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit. Vivamus consequat eu nulla nec commodo. Praesent leo quam, luctus sit 
+                        amet libero in, mattis efficitur leo. Suspendisse sodales ex non vehicula sagittis. Nulla euismod 
+                        aliquam erat, ut viverra magna tristique in. In quis dolor a justo gravida sollicitudin. Maecenas 
+                        vehicula lectus at purus sagittis vulputate. Nunc lobortis nulla nunc, eu tristique erat suscipit 
+                        in. Quisque lacinia quam tincidunt, faucibus ligula sed, porta urna. Etiam dapibus mauris nulla, 
+                        eget egestas est varius vitae.
+                    </p>
+                    <img src={graines}/>
+                </div>
+            </section>
+
         </div>
-        <div>
-          <img src={img2} />
-        </div>
-        <div>
-          <img src={img3} />
-        </div>
-      </Carousel>
-      {/* <h1>Qui sommes nous ?</h1> */}
-    </div>
-  );
+    );
 }
