39a353e9859c1e63053c72069636e8d4e16bcb54
Alexandre Pedro Monteiro <alexandre.pedro-monteiro@epitech.eu>
2022-01-27 11:14:09 +0100

    [MODIFY/ADD] sub step

---
+++ client/src/components/PaymentSubscribe.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+
+export default function PaymentSubscribe(props) {
+  console.log(props);
+  return <h1>Payement</h1>;
+}
--- client/src/components/SubscribeMysteryBox.jsx
+++ client/src/components/SubscribeMysteryBox.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
+// import Modal from "react-modal";
+import ReactDOM from "react-dom";
 
 export default function SubscribeMysteryBox() {
+  const [option, setOption] = useState(null);
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <div>
       <div>
@@ -11,6 +23,7 @@ export default function SubscribeMysteryBox() {
           molestiae quibusdam reprehenderit accusantium qui est dolorum dolore
           quisquam. Quaerat.
         </p>
+        <button>Abonne-toi</button>
       </div>
       <div>
         <h2>Abonnement 2</h2>
@@ -20,6 +33,7 @@ export default function SubscribeMysteryBox() {
           labore maiores explicabo voluptatibus dolores voluptas natus ut, atque
           distinctio expedita quam ad?
         </p>
+        <button>Abonne-toi</button>
       </div>
       <div>
         <h2>Abonnement 3</h2>
@@ -29,6 +43,7 @@ export default function SubscribeMysteryBox() {
           non, libero harum facere possimus veritatis? Eaque corporis quas quo
           ipsam neque dignissimos.
         </p>
+        <button>Abonne-toi</button>
       </div>
     </div>
   );
