043ff98492d70bfd84dfd78694d98b930b96580f
Nolwenn <nolwenn.sibiril@epitech.eu>
2022-01-28 10:38:19 +0100

    [Update] homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ import img1 from "../assets/1.jpeg";
 import img2 from "../assets/2.jpeg";
 import img3 from "../assets/3.jpeg";
 import boxSeedo from "../assets/boxSeedo.jpg";
-import graines from "../assets/10-graines-bonnes-pour-la-sante.jpg";
+import seeds from "../assets/lineOfSeeds.png";
 
 export default function Home() {
     return (
@@ -32,6 +32,14 @@ export default function Home() {
                         consectetur adipiscing elit
                     </p>
                 </div>
+            </section>
+            <hr className='hrGreen'></hr>
+            <section className='vertical-flex center-flex marginAuto seeds'>
+                <img src={seeds} className='imgSeed'/>
+                <p className='textSeeds'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dignissim, sem id laoreet varius, 
+                    mauris sem porttitor orci, et vulputate leo ligula quis risus. Nam imperdiet imperdiet mollis. 
+                    Mauris elit leo, sollicitudin et elementum vitae, bibendum quis nulla. Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit.</p>
             </section>
             <hr className='hrGreen'></hr>
             <section className="about vertical-flex marginAuto">
